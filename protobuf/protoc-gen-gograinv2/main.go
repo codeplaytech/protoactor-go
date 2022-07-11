524e90e4a84bde407f0ff52a85b538027912e23c
cupen <xcupen@gmail.com>
2022-07-11 20:09:56 +0800

    wip:-

--- protobuf/protoc-gen-gograinv2/main.go
+++ protobuf/protoc-gen-gograinv2/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"log"
 	"strings"
 	"text/template"
 
@@ -22,13 +24,28 @@ func removePackagePrefix(name string, pname string) string {
 
 func generateCode(req *plugin.CodeGeneratorRequest, filenameSuffix string, goFmt bool) *plugin.CodeGeneratorResponse {
 
+	targetFiles := map[string]struct{}{}
+	for _, name := range req.FileToGenerate {
+		targetFiles[name] = struct{}{}
+	}
+
 	response := &plugin.CodeGeneratorResponse{}
 	for _, f := range req.GetProtoFile() {
+		showFileDescriptorProto(f)
+		name := f.GetName()
+		if _, isTarget := targetFiles[name]; !isTarget {
+			continue
+		}
+
+		if !strings.HasSuffix(name, ".proto") {
+			panic(fmt.Errorf("proto file must be with .proto suffix: %s, ", name))
+		}
+
 		s := generate(f)
 
 		// we only generate grains for proto files containing valid service definition
 		if len(f.GetService()) > 0 {
-			fileName := strings.Replace(f.GetName(), ".", "_", 1) + "actor.go"
+			fileName := strings.TrimSuffix(name, ".proto") + filenameSuffix
 			r := &plugin.CodeGeneratorResponse_File{
 				Content: &s,
 				Name:    &fileName,
@@ -46,11 +63,29 @@ func generate(file *google_protobuf.FileDescriptorProto) string {
 	pkg := ProtoAst(file)
 
 	t := template.New("grain")
-	t, _ = t.Parse(code)
+	t, err := t.Parse(code)
+	if err != nil {
+		panic(err)
+	}
 
 	var doc bytes.Buffer
-	t.Execute(&doc, pkg)
+	if err := t.Execute(&doc, pkg); err != nil {
+		panic(err)
+	}
 	s := doc.String()
-
 	return s
+}
+
+func showFileDescriptorProto(file *google_protobuf.FileDescriptorProto) {
+	log.Printf("===== file ======")
+	log.Printf("name=%v", file.GetName())
+	log.Printf("package=%v", file.GetPackage())
+	log.Printf("dependency=%v", file.Dependency)
+	log.Printf("PublicDependency=%v", file.PublicDependency)
+
+	log.Printf("---- messges ----")
+	for i, m := range file.GetMessageType() {
+		log.Printf("\t%d: %s", i+1, m.GetName())
+	}
+	log.Printf("=================")
 }
